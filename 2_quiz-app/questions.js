ff14bca690dcd0b8aaf7f86ad63d99c71e0a0bd5
sakuexe <saku.karttunen@student.hamk.fi>
2022-11-23 16:35:16 +0200

    got the answer-check mechanic working

--- 2_quiz-app/questions.js
+++ 2_quiz-app/questions.js
@@ -1,5 +1,22 @@
-Prompt1 = {
-    question: "What year was the cult classic movie, Shark Tale, released?",
-    wrongAnswers: ["1987", "2010", "2002"],
-    correct: "2004",
-}
+Prompts = [
+    {
+        question: "What year was the cult classic movie, Shark Tale, released?",
+        wrongAnswers: ["1987", "2010", "2002"],
+        correctAnswer: "2004",
+    },
+    {
+        question: "Who was the first Formula 1 World Champion",
+        wrongAnswers: ["Niki Lauda", "Mike Hawthorn", "Juan Manuel Fangio"],
+        correctAnswer: "Qiuseppe Farina",
+    },
+    {
+        question: "Which company was initially called 'Cadabra'?",
+        wrongAnswers: ["Samsung", "Paypal", "Walmart"],
+        correctAnswer: "Amazon",
+    },
+    {
+        question: "What country has the highest life expectancy?",
+        wrongAnswers: ["Japan", "Canada", "Finland"],
+        correctAnswer: "Hong Kong",
+    },
+]
--- 2_quiz-app/quizscript.js
+++ 2_quiz-app/quizscript.js
@@ -1,33 +1,66 @@
 const questionElement = document.querySelector('#quiz-question')
 const answers = document.querySelectorAll('.quiz-answer')
+const lockAnswer = document.querySelector('#lock-answer')
+const quizQuestions = Prompts
+let promptCounter = 0 // current question, counted in indexes
+let scoreCounter = 0
 
+shuffleArray(quizQuestions) // shuffle the order of questions
+
+// * Give elements eventlisteners
 for (let answer of answers) {
-    answer.parentElement.addEventListener('click', event => {
-        answers.forEach(answer => {answer.parentElement.classList.remove('chosen-answer')})
-        answer.parentElement.classList.add('chosen-answer')
-    })
+	answer.parentElement.addEventListener('click', _ => {
+		answers.forEach(answer => {
+			answer.parentElement.classList.remove('chosen-answer')
+		})
+		answer.parentElement.classList.add('chosen-answer')
+	})
 }
+
+lockAnswer.addEventListener('click', _ => {
+    checkAnswer()
+    console.log(scoreCounter)
+})
 
 // Set up question
 function getQuestion() {
-    questionElement.innerText = Prompt1.question
-    let currentQuestions = Prompt1.wrongAnswers
-    currentQuestions.push(Prompt1.correct)
-    console.log(currentQuestions)
-    shuffleArray(currentQuestions)
+	const currentPrompt = quizQuestions[promptCounter]
 
-    console.log(currentQuestions)
-    for (let index = 0; index < currentQuestions.length; index++) {
-        answers[index].innerText = currentQuestions[index]
-    }
+	console.table(quizQuestions)
+	// assign current question into the UI
+	questionElement.innerText = currentPrompt.question
+
+	// get current question's answers and assign them to an array
+	let currentAnswers = currentPrompt.wrongAnswers
+	// also push the correct answer in the array
+	currentAnswers.push(currentPrompt.correctAnswer)
+
+	// Shuffle the array
+	shuffleArray(currentAnswers)
+	console.table(currentAnswers)
+
+	for (let [index, answer] of currentAnswers.entries()) {
+		console.log(index, answer)
+		answers[index].innerText = answer
+	}
 }
 
 function checkAnswer() {
-    // TODO: check if chosen answer == Prompt.correct
+	// TODO: check if chosen answer == Prompt.correct
+    // search for the element with the class .chosen-answer
+    let chosenAnswer = document.querySelector('.chosen-answer')
+    if (!chosenAnswer) return
+    chosenAnswer = chosenAnswer.querySelector('.quiz-answer').innerText
+    console.log(chosenAnswer)
+
+    // check if the answer is the same as correct answer
+    const correctAnswer = quizQuestions[promptCounter].correctAnswer
+    if (correctAnswer !== chosenAnswer) return
+    scoreCounter += 1
 }
 
 getQuestion()
 
 function shuffleArray(arr) {
-  arr.sort(() => Math.random() - 0.5);
+	arr.sort(() => Math.random() - 0.5)
 }
