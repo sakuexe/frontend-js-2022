d9d68cb24cfff1913e6d95188d7c0b134f5a9d0d
Saku Karttunen <saku.karttunen@gmail.com>
2022-11-30 14:35:58 +0200

    small addition to objects notes

--- notes/12-objects.js
+++ notes/12-objects.js
@@ -48,6 +48,8 @@ String.prototype.entitify = function() {
     return this.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;")
 }
 
+Number.prototype.isEven = () => (this % 2 == 0) ? true : false
+
 console.log(mainVariable.lastCharacter(), "is the last character of mainVariable")
 console.log(hacker)
 console.log(hacker.entitify())
